9df4b32aff1060a2f3cefb02b1859996ec6cb0af
Rodrigo Bercini <bercini@pistildata.com>
2022-01-13 17:36:34 -0300

    Major project rework

--- cep_finder.py
+++ cep_finder.py
@@ -1,67 +1,80 @@
-from unicodedata import normalize
-from bs4 import BeautifulSoup
+import re
 import requests
+from bs4 import BeautifulSoup
+from unicodedata import normalize
 
 
-################################################
-#####
-# Retorna CEPs a partir de endereços completos do site Oficial dos Correios
-# Após 300~ CEPs, a extração é bloqueada temporariamente
-# Exemplo: cep_finder('Av Protásio Alves', '199', 'RS', 'Porto Alegre')
-#####
-################################################
+class CepFinder:
+    def __init__(self, address, number, city, uf, verbose=False):
+        self.address = address
+        self.number = number
+        self.city = city
+        self.uf = uf
+        self.verbose = verbose
 
-# Função para remover caracteres especiais
-def rm_diacritics(string):
-    return normalize('NFKD', string).encode('ASCII', 'ignore').decode('ASCII')
-
-def cep_finder(address, number, city, uf):     
-    try:     
-        data = {
-            'UF': uf,
-            'Localidade': rm_diacritics(city),
+    def _generate_json_data(self) -> dict:
+        '''
+        Generate the json data to be sent to the Correios website.
+        '''
+        return {
+            'UF': self.uf,
+            'Localidade': self._replace_diacritics(self.city),
             'Tipo': '',
-            'Logradouro': rm_diacritics(address),
-            'Numero': number
+            'Logradouro': self._replace_diacritics(self.address),
+            'Numero': self.number
         }
-        url = "http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaCep.cfm"
-        response = requests.post(url, data=data)
-        doc = BeautifulSoup(response.text, 'html.parser')
-        tags = doc.find_all('td') # Encontra todas as tags 'td'
-        cep = tags[3].text # Extrai o texto da terceira tag, onde se encontra o CEP
-        if cep[5] == '-': # Gerenciando erros
+
+    def _generate_complete_address_string(self) -> str:
+        '''
+        Generate the complete address string to be sent to Mapacep website.
+        '''
+        return f'{self.address} {self.number} {self.city} {self.uf}'
+
+    def _replace_diacritics(string) -> str:
+        '''
+        Replace a character with a diacritic with the same character without diacritic.
+        Input: 'Álvaro'
+        Output: 'Alvaro'
+        '''
+        return normalize('NFKD', string).encode('ASCII', 'ignore').decode('ASCII')
+    
+    def _validate_cep(self, cep, doc):
+        if re.match('^\d{5}-\d{3}$', cep):
             return cep
+        elif self.verbose:
+            print('Error while scraping the request')
+            return None
         else:
-            return 'Erro'
-    except:
-        return 'Erro'
+            return None
 
-
-################################################
-##### Alternativa ao cep_finder()
-# Retorna CEPs a partir de endereços completos do site MapaCep. Também há um limite para extração
-# Example: cep_finder_mapacep('Av Protasio Alves', '199','Porto Alegre' , 'RS')
-#####
-################################################
-
-
-def cep_finder_mapacep(address,number,city,uf):    
-    try:
-        complete_address = ' '.join([address, str(number), city, uf])
+    def find_cep_mapacep(self) -> str:
+        '''
+        Get the CEP of an address using www.mapacep.com.br, limited to a few CEPs (not sure about the exact limit).
+        '''
         data = {
-            'keywords': complete_address,
+            'keywords': self._generate_complete_address_string(),
             'submit': 'pesquisar'
         }
         url = "https://www.mapacep.com.br/"
         response = requests.post(url, data=data)
         doc = BeautifulSoup(response.text, 'html.parser')
         docx = doc.find_all('p')
-        docx_string = str(docx[2]) # Seleciona a segunda tag 'p', onde se encontra o CEP
-        lens = len(docx_string) # Prepara variável para slicing
-        cep = docx_string[(lens-21):(lens-12)] # Extrai o CEP através de slicing reverso
-        if cep[5] == '-': # Gerenciando Erros
-            return cep
-        else:
-            return 'Erro'
-    except:
-        return 'Erro'
+        docx_string = str(docx[2]) # Select the second <p> tag, where the CEP is located
+        lens = len(docx_string) 
+        cep = docx_string[(lens-21):(lens-12)] # Get the CEP through reverse slicing
+        
+        return self._validate_cep(cep, doc)
+
+    def find_cep_correios_website(self) -> str:
+        '''
+        Deprecated as the original endpoint was shutdown by Correios.
+        Scrapes CEP by address from the Correios Official Website. The website blocks scraping after ~300 requests
+        '''  
+        data = self._generate_json_data()
+        url = "http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaCep.cfm"
+        response = requests.post(url, data=data)
+        doc = BeautifulSoup(response.text, 'html.parser')
+        tags = doc.find_all('td')
+        cep = tags[3].text
+
+        return self._validate_cep(cep, doc)
